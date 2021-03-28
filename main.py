0908d3359fb98b24f7b7f7d4aaf70ebf82c2abc7
cabbagepsyduck <76392932+cabbagepsyduck@users.noreply.github.com>
2021-03-28 13:14:01 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,82 +1,28 @@
-import gym
-import pix_sample_arena
-import time
-import pybullet as p
-import pybullet_data
-import cv2
-import os
 import numpy as np
+import cv2
+import math
 
-
-
-if __name__=="__main__":
-    lower = np.array([[0,227,0],[145,0,0],[227,227,0],[211,114,211]])  # 1.Green , 2.Red , 3.Yellow ,4. Pink (Patient)
-    
-
-    env = gym.make("pix_sample_arena-v0")
-    time.sleep(3)
-    img = env.camera_feed()
-    img = img[100:412,100:412]
-    cv2.imshow("img", img)
-    # img = img[]
-    img = cv2.resize(img, (600,600))
-    # cv2.imshow("test", img2)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    cv2.imshow("hsv",hsv)
-
-    mask = cv2.inRange(img , lower[0],lower[0])
-    cv2.imshow("threshold", mask)
-    Fmask = cv2.bitwise_and(img, img, mask = mask)
-    # Fgreen[green == 0] = 255
-    cv2.imshow("masked", Fmask)
-
-    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    mat = np.zeros((6,6))                                                                  #Square area = 46
-  
-
-    print(cv2.contourArea(contours[0]))
-    i=0
-    j=0
-    p=0
-
-    for i in range(6):
-        
-        for j in range(6):     
-            print(img[50 + 100*i,50+100*j])                   
-            if(img[50 + 100*i,50+100*j].all()==lower[0].all()):
-                mat[j][i]=1
-            j=j+1 
-
-    i=i+1
-
-
-
-
-
-
-   
-    print(mat)
-    print(np.shape(img))
-    # print(matrix)
-    # cv2.drawContours(gray, contours, -1, (0, 255, 0), 3)
-    # cv2.imshow("img2",gray)
-
-  
-    # i = 0
-
-
-
-
-    
-
-
-
-    cv2.waitKey(0)
-
-    
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    time.sleep(100)
-
-
+img = cv2.imread('sample.jpg')
+img = cv2.resize(img, (360,360))
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+lower = np.array([[55,150,150],[145,0,0],[227,227,0],[211,114,211]])  #0.Green , 1.Red , 2Yellow ,3Pink (Patient)
+upper = np.array([[65,255,255],[145,0,0],[227,227,0],[211,114,211]])  #0.Green , 1.Red , 2Yellow ,3Pink (Patient)
+mat = np.zeros((6,6))
+maskg=cv2.inRange(hsv, lower[0], upper[0])
+Gmask = cv2.bitwise_and(img, img, mask = maskg)
+greencont,_ = cv2.findContours(maskg, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+for cnt in greencont:
+	M = cv2.moments(cnt)
+	cx = int(M['m10']/M['m00'])
+	cy = int(M['m01']/M['m00'])
+	print('{} {}'.format(cx, cy))
+	x = math.ceil(cx/60) - 1
+	y = math.ceil(cy/60) - 1
+	mat[y][x] = 1
+	
+print(mat)
+print(len(greencont))
+cv2.drawContours(img, greencont, 0, (50,50,50),3)
+cv2.imshow('original', img)
+cv2.imshow('maskg', Gmask)
+cv2.waitKey(0)	
